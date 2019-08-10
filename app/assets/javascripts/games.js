8296bb1f5033ee75d0b456fe8a191a883f21f592
Erin Stalzer <erinstalzer@gmail.com>
2019-08-10 15:34:55 -0400

    update displayCreateForm() add displayGames()

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -3,6 +3,7 @@ const BASE_URL = "http://localhost:3000"
 function displayCreateForms() {
     let gameFormDiv = document.getElementById("games-form")
     let html = '<form onClick="createForm(); return false"><label>Name</label><input type="text" id="name"><br><label>Min play time</label><input placeholder="in minutes" type="text" id="min_play_time"><label>Max play time</label><input placeholder="in minutes" type="text" id="max_play_time"><br><label>Min # of players</label><input type="text" id="min_num_players"><label>Max # of players</label><input type="text" id="max_num_players"><br><label>Min age</label><input type="text" id="min_age"><label>Max age</label><input placeholder="enter '100' if no max" type="text" id="max_age"><br><input type="submit" value="Create Game"></form>'
+    gameFormDiv.innerHTML = html
 }
 
 function getGames(){
@@ -21,4 +22,9 @@ function getGames(){
 function clearCreateForm() {
     let gameFormDiv = document.getElementById("games-form")
     gameFormDiv.innerHTML = ''
+}
+
+function displayGames(){
+    e.preventDefault()
+    clearCreateForm()
 }
