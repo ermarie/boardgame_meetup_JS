d78174417c41240391c03da48929b02677a25504
Erin Stalzer <erinstalzer@gmail.com>
2019-08-10 14:46:46 -0400

    add HTML to games index view page

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -3,5 +3,18 @@ function displayCreateForms() {
 }
 
 function getGames(){
+    clearPage()
+    let all_games = document.getElementById("all-games")
+    all_games.innerHTML = "<ul>"
+    fetch(BASE_URL + "games.json")
+    .then(resp => response.json())
+    .then(games => {
+        all_games.innerHTML += games.map(game => <li><a href="#" data-id="${game.id}">${game.name}</a></li>)
+        all_games.innerHTML = "</ul>"
+    })
     
+}
+
+function clearPage() {
+
 }
