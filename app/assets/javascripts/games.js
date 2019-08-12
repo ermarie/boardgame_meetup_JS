5d7ddcd691b039d21d165f85917381828abf10a9
Erin Stalzer <erinstalzer@gmail.com>
2019-08-12 11:00:40 -0400

    troubleshoot issue with getGames()

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -2,18 +2,20 @@ const BASE_URL = "http://localhost:3000"
 
 function displayCreateForm() {
     let gameFormDiv = document.getElementById("games-form")
-    let html = '<form onClick="createForm(); return false"><label>Name</label><input type="text" id="name"><br><label>Min play time</label><input placeholder="in minutes" type="text" id="min_play_time"><label>Max play time</label><input placeholder="in minutes" type="text" id="max_play_time"><br><label>Min # of players</label><input type="text" id="min_num_players"><label>Max # of players</label><input type="text" id="max_num_players"><br><label>Min age</label><input type="text" id="min_age"><label>Max age</label><input placeholder="enter '100' if no max" type="text" id="max_age"><br><input type="submit" value="Create Game"></form>'
+    let html = '<form onClick="createForm(); return false"><label>Name</label><input type="text" id="name"><br><label>Min play time</label><input placeholder="in minutes" type="text" id="min_play_time"><label>Max play time</label><input placeholder="in minutes" type="text" id="max_play_time"><br><label>Min # of players</label><input type="text" id="min_num_players"><label>Max # of players</label><input type="text" id="max_num_players"><br><label>Min age</label><input type="text" id="min_age"><label>Max age</label><input placeholder="enter 100 if no max" type="text" id="max_age"><br><input type="submit" value="Create Game"></form>'
     gameFormDiv.innerHTML = html
 }
 
 function getGames(){
     clearCreateForm()
-    let all_games = document.getElementById("all-games")
-    all_games.innerHTML = "<ul>"
-    fetch(BASE_URL + "games.json")
-    .then(resp => response.json())
+    
+    fetch(BASE_URL + "/games.json")
+    .then(response => response.json())
     .then(games => {
-        all_games.innerHTML += games.map(game => `<li><a href="#" data-id="${game.id}">${game.name}</a></li>`.join(""))
+        let all_games = document.getElementById("all-games")
+        all_games.innerHTML = "<ul>"
+        all_games.innerHTML += games.map(game => {
+            return `<li><a href="#" data-id="${game.id}">${game.name}</a></li>`}).join("")
         all_games.innerHTML = "</ul>"
     })
 }
